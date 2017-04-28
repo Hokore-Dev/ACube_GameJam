5414543e10d88570759ac89b818379ae30db991b
bbangwon <bbangwon.chung@gmail.com>
2017-04-29 02:15:14 +0900

    UI작업중

---
+++ Assets/Script/UI/THGageController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(UnityEngine.UI.Slider))]
+public class THGageController : MonoBehaviour {
+
+    protected Slider slider;
+    protected int maxValue;
+    protected int currentValue;
+
+	// Use this for initialization
+	protected void Start () {
+        slider = GetComponent<Slider>();
+    }
+
+    public void InitValue(int max_value, int current_value)
+    {
+        maxValue = max_value;
+        currentValue = current_value;
+    }
+
+    public void InitValue(int max_value, bool full_gage = true)
+    {
+        if (full_gage)
+            InitValue(max_value, max_value);
+        else
+            InitValue(max_value, 0);
+    }
+
+    public void SetValue(int current_value)
+    {
+        currentValue = current_value;
+    }
+
+    protected void OnGUI()
+    {
+        slider.value = (float)((float)currentValue / (float)maxValue);
+        if (currentValue == 0)
+            slider.fillRect.gameObject.SetActive(false);
+        else
+            slider.fillRect.gameObject.SetActive(true);
+    }
+}
+
---
+++ Assets/Script/UI/THIncreseNum.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(UnityEngine.UI.Text))]
+public class THIncreseNum : MonoBehaviour {
+
+    [SerializeField]
+    private int currentValue = 0;
+
+    [SerializeField]
+    private string addString = "";
+
+    [SerializeField]
+    private float duration = 0.5f;
+
+    Text numText;
+    // Use this for initialization
+    void Start () {
+        numText = GetComponent<Text>();
+        StartIncreseNum(5000);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void StartIncreseNum(int target)
+    {
+        StopCoroutine("CountTo");
+        StartCoroutine("CountTo", target);
+    }
+
+    private void OnGUI()
+    {
+        numText.text = currentValue.ToString() + addString;
+    }
+
+    IEnumerator CountTo(int target)
+    {
+        int start = currentValue;
+        for (float timer = 0; timer < duration; timer += Time.deltaTime)
+        {
+            float progress = timer / duration;
+            currentValue = (int)Mathf.Lerp(start, target, progress);
+            yield return null;
+        }
+        currentValue = target;
+    }
+
+
+
+
+}
