39733c5a733bf6da357a97a78c0b1d9f293cc40c
bbangwon <bbangwon.chung@gmail.com>
2017-04-29 12:50:12 +0900

    UI작업중

---
+++ Assets/Script/UI/THHPGage.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class THHPGage : THGageController
+{
+    public Color manyHP;
+    public Color midumeHP;
+    public Color minHP;
+
+	// Use this for initialization
+	new void Start () {
+        base.Start();
+	}
+
+    new void OnGUI()
+    {
+        HPColorChange();
+        base.OnGUI();
+    }
+
+    void HPColorChange()
+    {
+        float currentHPRate = (float)THHPManager.Instance.CurrentHP / (float)THGameSetting.Instance.maxHP;
+        Debug.Log(currentHPRate);
+        if (currentHPRate > 0.7f)
+        {
+            slider.fillRect.GetComponent<UnityEngine.UI.Image>().color = manyHP;
+        }
+        else if (currentHPRate > 0.3f)
+        {
+            slider.fillRect.GetComponent<UnityEngine.UI.Image>().color = midumeHP;
+        }
+        else
+        {
+            slider.fillRect.GetComponent<UnityEngine.UI.Image>().color = minHP;
+        }
+    }
+}
